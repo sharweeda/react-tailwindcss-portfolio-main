b615b6fb08bc2313cf2a803d3c15feed43157692
Mahmoud SharWeeDa <mahmoudsharweeda9840n@gmail.com>
2025-09-12 14:30:24 +0300

    first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,18 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: "I’m a passionate Front-End Developer from Cairo, Egypt 🇪🇬. I specialize in building modern, responsive, and user-friendly websites."
+	},
+	{
+		id: 2,
+		bio: "With 2 years of experience, I’ve worked on different projects that helped me improve my skills in React.js, Next.js, Tailwind CSS, and JavaScript (ES6+)."
+	},
+	{
+		id: 3,
+		bio: "I love turning creative ideas into real digital experiences. Besides coding, I’m also interested in design and making UIs look clean and smooth."
+	},
+	{
+		id: 4,
+		bio: "My goal is to keep learning, growing, and contributing to projects that make an impact ."
+	}
+];
